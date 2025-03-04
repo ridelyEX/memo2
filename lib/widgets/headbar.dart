18cd45e24280a0c054951105888b833fd2737737
MFd13 <dylanmf1305@gmail.com>
2025-03-03 23:08:29 -0600

    metodo temporizador, falta implementarlo xd

--- lib/widgets/headbar.dart
+++ lib/widgets/headbar.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../config/config.dart';
@@ -16,16 +18,45 @@ class Headbar extends StatefulWidget {
 class _HeadbarState extends State<Headbar> {
 
   int? cont, mov, pares;
+  int seg=0,min=0;
   bool? par = false;
+
+  void _contadorTiempo() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        seg++;
+        if (seg>60) {
+          seg=0;
+          min++;
+        }
+      });
+    });
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _contadorTiempo();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.blueGrey[900],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Tiempo: ${min.toString().padLeft(2, '0')}:${seg.toString().padLeft(2, '0')}",
+            style: TextStyle(color: Colors.black, fontSize: 18),
+          ),
+        ],
+      ),
+    );
   }
 }
